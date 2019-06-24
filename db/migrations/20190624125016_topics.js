f2d3f11eaa2c16513abd67c84662e1d40fc61044
Dikasteria <rjhey@hotmail.com>
2019-06-24 15:20:04 +0100

    Migration completed

---
+++ db/migrations/20190624125016_topics.js
@@ -0,0 +1,15 @@
+exports.up = function(knex, Promise) {
+  console.log("Creating topics table...");
+  return knex.schema.createTable("topics", topicsTable => {
+    topicsTable
+      .string("slug")
+      .unique()
+      .primary();
+    topicsTable.string("description").notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  console.log("Deleting topics table...");
+  return knex.schema.dropTable("topics");
+};
---
+++ db/migrations/20190624125027_users.js
@@ -0,0 +1,16 @@
+exports.up = function(knex, Promise) {
+  console.log("Creating users table...");
+  return knex.schema.createTable("users", usersTable => {
+    usersTable
+      .string("username")
+      .unique()
+      .notNullable();
+    usersTable.string("avatar_url").notNullable();
+    usersTable.string("name").notNullable();
+  });
+};
+
+exports.down = function(knex, Promise) {
+  console.log("Deleting users table");
+  return knex.schema.dropTable("users");
+};
